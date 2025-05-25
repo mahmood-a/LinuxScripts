5c15d8b4bb1fc2fb4b988d46e83a668423c427b8
Mahmood Abdulla <mahmood.abdula@gmail.com>
2025-05-25 17:20:41 +0300

    fixed package name: kdb --> kbd

--- setuplargefont.sh
+++ setuplargefont.sh
@@ -1,5 +1,5 @@
 # This script sets a larger font for Alpine Linux Terminal, just to slow down screwing up your eyes
-apk add kdb
+apk add kbd
 sed -i 's/^consolefont=".*"$/consolefont="solar24x32.psfu.gz"/' /etc/conf.d/consolefont
 rc-update add consolefont boot
 rc-update consolefont restart
